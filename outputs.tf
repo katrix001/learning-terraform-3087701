0b8738f0fb38472aeba24d44c73406ace34867a3
katrix001 <60002028+katrix001@users.noreply.github.com>
2024-07-18 13:49:00 +0530

    added some output variables

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,9 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.blog.arn
 }
+output "public_dns" {
+  value = aws_instance.blog.public_dns
+}
+output "sec_gr_id" {
+  value = module.blog_sg_security-group.security_group_id
+}
